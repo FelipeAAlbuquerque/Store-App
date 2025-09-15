b31394f6b951f00dd0092818fc7d2fad4379abd9
Felipe <kikoalbuquerque02@gmail.com>
2025-09-15 11:21:11 -0300

    Established communication between front ui and back end app

--- src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -4,6 +4,7 @@ package com.eazybytes.eazystore.controller;
 import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
