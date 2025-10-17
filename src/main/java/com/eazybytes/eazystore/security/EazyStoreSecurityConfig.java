a3fbe86f692928d76c0a42b0665b355569b7a1f4
Felipe <kikoalbuquerque02@gmail.com>
2025-10-17 09:52:05 -0300

    Added JWT token validation filter

--- src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -4,16 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -42,32 +42,27 @@ public class EazyStoreSecurityConfig {
                             requests.anyRequest().authenticated();
                         }
                 )
+                .addFilterBefore(new JWTTokenValidatorFilter(publicPaths), BasicAuthenticationFilter.class)
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults()).build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var user1 = User.builder().username("felipe")
-                .password("$2a$12$vsi964EvG80PyZfavERimu1jX7rbTNjjj7SaGr8waXq3zwZX2kXqm").roles("USER").build();
-        var user2 = User.builder().username("admin")
-                .password("$2a$12$neiGI7viF8rIb8YBgniWruvNZSQNqtG4F3Mt3631ehov0BEk/N9vO").roles("USER","ADMIN").build();
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
 
     @Bean
     public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        var daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        var providerManager = new ProviderManager(daoAuthenticationProvider);
+            AuthenticationProvider authenticationProvider) {
+        var providerManager = new ProviderManager(authenticationProvider);
         return providerManager;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 
     @Bean
