5517994547a1f9ece2de7e5e66f909092520b427
Felipe <kikoalbuquerque02@gmail.com>
2025-10-17 10:03:25 -0300

    Auditor to get information from authenticated principal

--- src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazystore.security;
 
+import com.eazybytes.eazystore.filter.JWTTokenValidatorFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
