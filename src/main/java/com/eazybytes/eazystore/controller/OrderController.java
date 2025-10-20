6471f5415b41863cc760783ae9ac27a7ffea3d1b
Felipe <kikoalbuquerque02@gmail.com>
2025-10-20 10:01:54 -0300

    New controller for oders placed on the ecommerce

---
+++ src/main/java/com/eazybytes/eazystore/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.eazybytes.eazystore.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final IOrderService iOrderService;
+
+    @PostMapping
+    public ResponseEntity<String> createOrder(@RequestBody  OrderRequestDto requestDto) {
+        iOrderService.createOrder(requestDto);
+        return ResponseEntity.ok("Order created successfully!");
+    }
+}
