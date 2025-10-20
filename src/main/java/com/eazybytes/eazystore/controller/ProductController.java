b89b9a7995d0abaacf60c46241a4cf9020f64544
Felipe <kikoalbuquerque02@gmail.com>
2025-10-20 15:40:29 -0300

    .

--- src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -7,10 +7,7 @@ import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
