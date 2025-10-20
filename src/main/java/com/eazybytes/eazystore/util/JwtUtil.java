e5d1ae63e6ea668ca5639fd9ab06d9fcfcdfb393
Felipe <kikoalbuquerque02@gmail.com>
2025-10-20 16:08:40 -0300

    Improved utility for better jwt token management

--- src/main/java/com/eazybytes/eazystore/util/JwtUtil.java
+++ src/main/java/com/eazybytes/eazystore/util/JwtUtil.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -34,7 +33,7 @@ public class JwtUtil {
                 .claim("roles", authentication.getAuthorities().stream().map(
                         GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
                 .issuedAt(new java.util.Date())
-                .expiration(new java.util.Date((new java.util.Date()).getTime() + 60 * 60 * 1000))
+                .expiration(new java.util.Date((new java.util.Date()).getTime() + 24 * 60 * 60 * 1000))
                 .signWith(secretKey).compact();
         return jwt;
     }
