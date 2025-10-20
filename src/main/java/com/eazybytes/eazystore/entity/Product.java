db19e31493aa813d7cf9635d1b9cf2c1f37a5234
Felipe <kikoalbuquerque02@gmail.com>
2025-10-20 15:50:29 -0300

    Minor changes to the Product model

--- src/main/java/com/eazybytes/eazystore/entity/Product.java
+++ src/main/java/com/eazybytes/eazystore/entity/Product.java
@@ -3,8 +3,10 @@ package com.eazybytes.eazystore.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Getter
 @Setter
