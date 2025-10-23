c548d4c0dc44185d9bb19d814a57756fc3678413
Felipe <kikoalbuquerque02@gmail.com>
2025-10-23 11:00:14 -0300

    Added new operations to get customers oders and orders status

--- src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
+++ src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
@@ -1,6 +1,14 @@
 package com.eazybytes.eazystore.repository;
 
+import com.eazybytes.eazystore.entity.Customer;
 import com.eazybytes.eazystore.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByCustomerOrderByCreatedAtDesc(Customer customer);
+
+    List<Order> findByOrderStatus(String orderStatus);
+}
