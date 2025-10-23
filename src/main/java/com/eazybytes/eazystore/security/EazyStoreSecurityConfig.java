aa0efe0a597372384b7926c012f77ce4b0bd860b
Felipe <kikoalbuquerque02@gmail.com>
2025-10-23 11:04:45 -0300

    minor adjustements

--- src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -45,6 +45,9 @@ public class EazyStoreSecurityConfig {
                             publicPaths.forEach(path ->
                                     requests.requestMatchers(path).permitAll());
                             requests.requestMatchers("/api/v1/admin/**").hasRole("ADMIN");
+                            requests.requestMatchers("/eazystore/actuator/**").hasRole("OPS_ENG");
+                            requests.requestMatchers("/swagger-ui.html", "/swagger-ui/**",
+                                    "/v3/api-docs/**").hasAnyRole("DEV_ENG","QA_ENG");
                             requests.anyRequest().hasAnyRole("USER", "ADMIN");
                         }
                 )
