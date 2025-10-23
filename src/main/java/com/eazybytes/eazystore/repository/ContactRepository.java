242e3715138ba9005377e4b97909903f5aa917d7
Felipe <kikoalbuquerque02@gmail.com>
2025-10-23 11:01:55 -0300

    new operation to get status from customer

--- src/main/java/com/eazybytes/eazystore/repository/ContactRepository.java
+++ src/main/java/com/eazybytes/eazystore/repository/ContactRepository.java
@@ -3,4 +3,9 @@ package com.eazybytes.eazystore.repository;
 import com.eazybytes.eazystore.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository extends JpaRepository<Contact, Long> {}
+import java.util.List;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    List<Contact> findByStatus(String status);
+}
