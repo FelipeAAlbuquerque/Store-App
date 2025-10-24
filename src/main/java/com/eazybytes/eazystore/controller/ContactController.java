0c3af29723ad38152994f6792d497a11c4100356
Felipe <kikoalbuquerque02@gmail.com>
2025-10-24 18:11:19 -0300

    Dto for contact information

--- src/main/java/com/eazybytes/eazystore/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazystore/controller/ContactController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/contacts")
@@ -17,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactController {
 
     private final IContactService iContactService;
+    private final ContactInfoDto contactInfoDto;
 
     @PostMapping
     public ResponseEntity<String> saveContact(
@@ -25,5 +23,12 @@ public class ContactController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Request processed successfully");
     }
+
+    @GetMapping
+    public ResponseEntity<ContactInfoDto> getContactInfo() {
+        return ResponseEntity.ok(contactInfoDto);
+    }
+
+
 
 }
